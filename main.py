3f921b87898a3f3cce71f82c0420a94c7293fb4b
WeizhuQIAN <36293978+WeizhuQIAN@users.noreply.github.com>
2023-06-19 21:12:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+
+import torch
+import numpy as np
+import torch.nn as nn
+import argparse
+import configparser
+from datetime import datetime
+from model.AGCRN_UQ import AGCRN_UQ as Network
+from model.BasicTrainer import Trainer
+from lib.TrainInits import init_seed
+from lib.dataloader import get_dataloader
+from lib.TrainInits import print_model_parameters
+from tqdm import tqdm
+from copy import deepcopy
+import torch.nn.functional as F
+import torchcontrib
+from model.train_methods import awa_train_combined, train_cali_mcx
+from model.test_methods import combined_test
+
+
+#*************************************************************************#
+Mode = 'train'
+DEBUG = 'True'
+DATASET = 'PEMS08'      #PEMS04/8/3/7
+DEVICE = 'cuda:0'
+MODEL = 'AGCRN'
+MODEL_NAME = "combined"#"combined/basic/dropout/heter"
+
+#get configuration
+config_file = 'model/{}_{}.conf'.format(DATASET, MODEL)
+#print('Read configuration file: %s' % (config_file))
+config = configparser.ConfigParser()
+config.read(config_file)
+
+from lib.utils import enable_dropout,save_model_,load_model_
+from lib.metrics import All_Metrics
+
+
+
+from lib.metrics import MAE_torch
+def masked_mae_loss(scaler, mask_value):
+    def loss(preds, labels):
+        if scaler:
+            preds = scaler.inverse_transform(preds)
+            labels = scaler.inverse_transform(labels)
+        mae = MAE_torch(pred=preds, true=labels, mask_value=mask_value)
+        return mae
+    return loss
+
+#parser
+args = argparse.ArgumentParser(description='arguments')
+args.add_argument('--dataset', default=DATASET, type=str)
+args.add_argument('--mode', default=Mode, type=str)
+args.add_argument('--device', default=DEVICE, type=str, help='indices of GPUs')
+args.add_argument('--debug', default=DEBUG, type=eval)
+args.add_argument('--model', default=MODEL, type=str)
+args.add_argument('--cuda', default=True, type=bool)
+#data
+args.add_argument('--val_ratio', default=config['data']['val_ratio'], type=float)
+args.add_argument('--test_ratio', default=config['data']['test_ratio'], type=float)
+args.add_argument('--lag', default=config['data']['lag'], type=int)
+args.add_argument('--horizon', default=config['data']['horizon'], type=int)
+args.add_argument('--num_nodes', default=config['data']['num_nodes'], type=int)
+args.add_argument('--tod', default=config['data']['tod'], type=eval)
+args.add_argument('--normalizer', default=config['data']['normalizer'], type=str)
+args.add_argument('--column_wise', default=config['data']['column_wise'], type=eval)
+args.add_argument('--default_graph', default=config['data']['default_graph'], type=eval)
+#model
+args.add_argument('--input_dim', default=config['model']['input_dim'], type=int)
+args.add_argument('--output_dim', default=config['model']['output_dim'], type=int)
+args.add_argument('--embed_dim', default=config['model']['embed_dim'], type=int)
+args.add_argument('--rnn_units', default=config['model']['rnn_units'], type=int)
+args.add_argument('--num_layers', default=config['model']['num_layers'], type=int)
+args.add_argument('--cheb_k', default=config['model']['cheb_order'], type=int)
+args.add_argument('--p1', default=config['model']['p1'], type=float)
+
+#train
+args.add_argument('--loss_func', default=config['train']['loss_func'], type=str)
+#args.add_argument('--loss_func', default='mse', type=str)
+args.add_argument('--seed', default=config['train']['seed'], type=int)
+args.add_argument('--batch_size', default=config['train']['batch_size'], type=int)
+args.add_argument('--epochs', default=config['train']['epochs'], type=int)
+#args.add_argument('--epochs', default=500, type=int)
+args.add_argument('--lr_init', default=config['train']['lr_init'], type=float)
+#args.add_argument('--lr_init', default=1e-2, type=float)
+args.add_argument('--lr_decay', default=config['train']['lr_decay'], type=eval)
+args.add_argument('--lr_decay_rate', default=config['train']['lr_decay_rate'], type=float)
+args.add_argument('--lr_decay_step', default=config['train']['lr_decay_step'], type=str)
+args.add_argument('--early_stop', default=config['train']['early_stop'], type=eval)
+args.add_argument('--early_stop_patience', default=config['train']['early_stop_patience'], type=int)
+args.add_argument('--grad_norm', default=config['train']['grad_norm'], type=eval)
+args.add_argument('--max_grad_norm', default=config['train']['max_grad_norm'], type=int)
+args.add_argument('--teacher_forcing', default=False, type=bool)
+args.add_argument('--tf_decay_steps', default=2000, type=int, help='teacher forcing decay steps')
+args.add_argument('--real_value', default=config['train']['real_value'], type=eval, help = 'use real value for loss calculation')
+#test
+args.add_argument('--mae_thresh', default=config['test']['mae_thresh'], type=eval)
+args.add_argument('--mape_thresh', default=config['test']['mape_thresh'], type=float)
+#log
+args.add_argument('--log_dir', default='./', type=str)
+args.add_argument('--log_step', default=config['log']['log_step'], type=int)
+args.add_argument('--plot', default=config['log']['plot'], type=eval)
+args.add_argument('--model_name', default=MODEL_NAME, type=str)
+
+
+
+args = args.parse_args([])
+init_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.set_device(int(args.device[5]))
+else:
+    args.device = 'cpu'
+
+#init model
+model = Network(args).to(args.device)
+for p in model.parameters():
+    if p.dim() > 1:
+        nn.init.xavier_uniform_(p)
+    else:
+        nn.init.uniform_(p)
+print_model_parameters(model, only_num=False)
+
+#load dataset
+train_loader, val_loader, test_loader, scaler = get_dataloader(args,
+                                                               normalizer=args.normalizer,
+                                                               tod=args.tod, dow=False,
+                                                               weather=False, single=False)
+#init loss function, optimizer
+if args.loss_func == 'mask_mae':
+    loss = masked_mae_loss(scaler, mask_value=0.0)
+elif args.loss_func == 'mae':
+    loss = torch.nn.L1Loss().to(args.device)
+elif args.loss_func == 'mse':
+    loss = torch.nn.MSELoss().to(args.device)
+else:
+    raise ValueError
+
+optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr_init, eps=1.0e-8,
+                             weight_decay=1e-6, amsgrad=False)
+
+#learning rate decay
+lr_scheduler = None
+if args.lr_decay:
+    print('Applying learning rate decay.')
+    lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(','))]
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,
+                                                        milestones=lr_decay_steps,
+                                                        gamma=args.lr_decay_rate)
+#start training
+trainer = Trainer(model, loss, optimizer, train_loader, val_loader, test_loader, scaler,
+                  args, lr_scheduler=lr_scheduler)
+
+
+### Pre-traning
+trainer.train()
+
+
+### AWA Re-training 
+
+#trainer.model = awa_train_combined(trainer,epochs=20)
+
+
+### Model Calibration
+
+T = train_cali_mc(trainer.model,10, args, val_loader, scaler)
+combined_test(trainer.model,10,trainer.args, trainer.test_loader, scaler,T)
+
+
+
+
+
